14b2e446e83b97d65ec04f646d757288100210f9
Anushika Suhani <anushika666@gmail.com>
2015-04-10 11:50:45 +0530

    Update CodeRipperzff.pl

--- CodeRipperzff.pl
+++ CodeRipperzff.pl
@@ -16,3 +16,23 @@ boole_or(1,0,1).
 boole_and(1,1,1).      
 boole_or(1,1,1).
 //sanduni
+
+
+
+
+
+//Suhani
+cr(J) :- search_vals(J,[],V),
+         reverse(V,Vars),
+         initial_assign(Vars,A),
+         write('  '), write(Vars), write('    '), write(J), nl,
+         write('---------------------------------------------'), nl,
+         write_row(J,Vars,A),
+         write('---------------------------------------------'), nl.
+
+write_row(J,Vars,A) :- write('  '), write(A), write('        '), 
+                       actual_vals(J,Vars,A,V), 
+write(V), nl,
+                       (successor(A,B) -> write_row(J,Vars,B) ; true).
+
+//Suhani
